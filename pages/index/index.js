e436cb0b566bdfecc99afe4e4192286baef3b89c
jason <guangweidu@qq.com>
2017-02-10 09:21:59 +0800

    update

---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+  },
+  //事件处理函数
+  bindViewTap: function () {
+  },
+  onLoad: function () {
+  }
+})
