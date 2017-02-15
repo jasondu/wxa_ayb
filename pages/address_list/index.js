9d68d8ff8cf03962865b090e8edd09b99227cc7d
jason <guangweidu@qq.com>
2017-02-15 17:46:54 +0800

    地址列表

--- pages/address_list/index.js
+++ pages/address_list/index.js
@@ -1,6 +1,8 @@
 // pages/address_list/index.js
 Page({
-  data:{},
+  data:{
+    first: true
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
@@ -15,5 +17,11 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
+  },
+
+  addAddress: function () {
+    wx.navigateTo({
+      url: '/pages/address/index'
+    })
   }
 })
