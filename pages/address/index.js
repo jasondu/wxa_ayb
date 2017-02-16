0d7d7f173966ecbeb44a49ac7fe8933fec310b5e
jason <guangweidu@qq.com>
2017-02-16 19:48:35 +0800

    添加地址和修改地址

--- pages/address/index.js
+++ pages/address/index.js
@@ -1,12 +1,32 @@
 // pages/address/index.js
-const { User, Query, Cloud } = require('../../libs/leancloud/av-weapp-min.js');
+const { User, Query, Cloud, Object } = require('../../libs/leancloud/av-weapp-min.js');
 Page({
   data: {
     address: '',
-    addressDetail: ''
+    address_detail: '',
+    keyToTitle: {
+      name: '联系人',
+      tel: '手机号',
+      address: '地址',
+      address_detail: '地址详情'
+    },
+    errorText: ''
   },
+  objectId: '',
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
+    console.log(options);
+    if (options.status == 'edit') {
+      // 编辑
+      this.objectId = options.id;
+      var query = new Query('Address');
+      query.get(options.id).then((address) => {
+        this.setData(address);
+      }, (error) => {
+        // 异常处理
+        console.error(error);
+      });
+    }
   },
   onReady: function () {
     // 页面渲染完成
@@ -22,6 +42,7 @@ Page({
   },
 
   getAddress: function () {
+    console.log('getAddress');
     var self = this;
     wx.chooseLocation({
       success: function (res) {
@@ -38,27 +59,76 @@ Page({
       }
     })
   },
+  formInput: function (name) {
+  },
+  int: null,
+  // 地址保存
   save: function (e) {
     console.log('formid: ' + e.detail.formId);
-    Cloud.run('sendWxaTemplate', {
-      openid: 'owLn50BTW-H9nIMGsnFzUvcDtt0w',
-      templateid: 'MfMVvu2FgaI-UJ1otWc5SlBsenUoewPRkI5en92zje4',
-      formid: e.detail.formId,
-      data: {
-        "keyword1": {
-          "value": "339208499",
-          "color": "#173177"
-        },
-        "keyword2": {
-          "value": "2015年01月05日 12:30",
-          "color": "#173177"
-        }
-      },
-      emphasis_keyword: 'keyword1.DATA'
-    }).then((data) => {
-      console.log(JSON.stringify(data));
-    }).catch(error => {
+    console.log(e.detail.value);
 
-    })
+    var formValue = e.detail.value;
+    var isPass = true;
+    var title = '';
+    for (var key in formValue) {
+      var value = formValue[key];
+      if (value == '') {
+        title = this.data.keyToTitle[key];
+        isPass = false;
+        break;
+      }
+    }
+    if (!isPass) {
+      this.setData({
+        errorText: title
+      });
+      if (this.int == null) {
+        this.int = setTimeout(() => {
+          this.setData({
+            errorText: ''
+          });
+          this.int = null;
+        }, 1000);
+      }
+    } else {
+      if (this.objectId == '') {
+        // 添加地址
+        var Address = Object.extend('Address');
+        var address = new Address();
+      } else {
+        // 修改地址
+        var address = Object.createWithoutData('Address', this.objectId);
+      }
+      address.set(formValue);
+      address.save().then(function (item) {
+        console.log('objectId is ' + item.id);
+        var app = getApp();
+        app.mustUpdate = true;
+        wx.navigateBack();
+      }, function (error) {
+        console.error(error);
+      });
+    }
+
+    // Cloud.run('sendWxaTemplate', {
+    //   openid: 'owLn50BTW-H9nIMGsnFzUvcDtt0w',
+    //   templateid: 'MfMVvu2FgaI-UJ1otWc5SlBsenUoewPRkI5en92zje4',
+    //   formid: e.detail.formId,
+    //   data: {
+    //     "keyword1": {
+    //       "value": "339208499",
+    //       "color": "#173177"
+    //     },
+    //     "keyword2": {
+    //       "value": "2015年01月05日 12:30",
+    //       "color": "#173177"
+    //     }
+    //   },
+    //   emphasis_keyword: 'keyword1.DATA'
+    // }).then((data) => {
+    //   console.log(JSON.stringify(data));
+    // }).catch(error => {
+
+    // });
   }
 })
--- pages/address_list/index.js
+++ pages/address_list/index.js
@@ -1,13 +1,20 @@
 // pages/address_list/index.js
+const { Query } = require('../../libs/leancloud/av-weapp-min.js');
 Page({
   data:{
-    first: true
+    first: true,
+    address: []
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
   onReady:function(){
     // 页面渲染完成
+    new Query('Address')
+      .descending('createdAt')
+      .find()
+      .then(address => this.setData({ address }))
+      .catch(console.error);
   },
   onShow:function(){
     // 页面显示
@@ -19,9 +26,14 @@ Page({
     // 页面关闭
   },
 
+  editAddress: function (e) {
+    wx.navigateTo({
+      url: '/pages/address/index?status=edit&id=' + e.currentTarget.dataset.id
+    })
+  },
   addAddress: function () {
     wx.navigateTo({
-      url: '/pages/address/index'
+      url: '/pages/address/index?status=add'
     })
   }
 })
